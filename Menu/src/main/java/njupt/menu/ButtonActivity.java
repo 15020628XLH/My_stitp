47102338125f5f98a83c76811f6f7d9dcdd752b9
XLDEPC\acer <1183219679@qq.com>
2018-05-04 14:04:10 +0800

    no message

---
+++ Menu/src/main/java/njupt/menu/ButtonActivity.java
@@ -0,0 +1,8 @@
+package njupt.menu;
+
+/**
+ * Created by acer on 2018/4/29.
+ */
+
+public class ButtonActivity extends MainActivity {
+}
---
+++ Menu/src/main/java/njupt/menu/photoActivity.java
@@ -0,0 +1,10 @@
+package njupt.menu;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by acer on 2018/4/29.
+ */
+
+public class photoActivity extends AppCompatActivity {
+}
